c3fc1f6bef7bfd08641f6f3c6c48b17ead5dacda
Kent Kawahara <kent.kawahara@verys.com>
2017-12-05 16:33:05 -0800

    login component created

---
+++ src/pages/login/Login.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+    
+    render() {
+        <div>
+            Login page will go here
+        </div>
+    }
+}

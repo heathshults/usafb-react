0ec09913a87d3823c08aa413ecb3e636d9149021
Kent Kawahara <kent.kawahara@verys.com>
2017-12-06 12:10:20 -0800

    added bootstrap css to application

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 
-import { Router } from 'react-router-dom'
-import { Switch, Route } from 'react-router'
+import { Router } from 'react-router-dom';
+import { Switch, Route } from 'react-router';
 
 import createHistory from 'history/createBrowserHistory';
 
 import store from 'services/redux/store';
 import Login from 'pages/login/Login';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 const history = createHistory();

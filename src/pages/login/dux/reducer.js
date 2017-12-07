8ef4f2a77dd31656f4fac61a1f15ca757a17cea1
Kent Kawahara <kent.kawahara@verys.com>
2017-12-07 13:54:31 -0800

    created action and reducer for login

---
+++ src/pages/login/dux/reducer.js
@@ -0,0 +1,19 @@
+import * as actions from './actions.js';
+
+const initialState = {
+  loggingIn: false,
+  loginError: ''
+};
+
+export default (state = initialState, action) => {
+  switch (action) {
+    case actions.LOGIN:
+      return { ...state, loggingIn: true, loginError: '' };
+    case actions.LOGGED_IN:
+      return { ...state, loggingIn: false };
+    case actions.LOGIN_ERROR:
+      return { ...state, loggingIn: false, loginError: action.payload };
+    default:
+      return initialState;
+  }
+};

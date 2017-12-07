92ece7131ae14123435d4111e445f8a2e17d0dcd
Kent Kawahara <kent.kawahara@verys.com>
2017-12-06 16:02:02 -0800

    created form.scss that follows ITCSS Scalable and Maintainable css patterns

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from 'pages/app/App';
+import 'styles/application.css';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 

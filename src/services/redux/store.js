fe27cd41aad599d6ea015985309cfb46b3e66311
Kent Kawahara <kent.kawahara@verys.com>
2017-12-07 14:00:59 -0800

    added redux saga middleware to store

--- src/services/redux/store.js
+++ src/services/redux/store.js
@@ -1,9 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
-// import thunkMiddleware from 'redux-thunk';
-// import history from './history.js';
-// import { routerMiddleware } from 'react-router-redux'
+import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
 
 export default createStore(
-  reducers
+  reducers,
+  applyMiddleware(createSagaMiddleware)
 );

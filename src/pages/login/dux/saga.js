3c512db9e94118bd7ed34774f4fefc54020a8a2d
Kent Kawahara <kent.kawahara@verys.com>
2017-12-07 17:12:07 -0800

    store token in local storage once logged in

--- src/pages/login/dux/saga.js
+++ src/pages/login/dux/saga.js
@@ -1,4 +1,5 @@
-import { take, call } from 'redux-saga/effects';
+import { take, call, put } from 'redux-saga/effects';
+import { push } from 'react-router-redux';
 
 import * as actions from './actions';
 import loginApi from './api';
@@ -13,10 +14,22 @@ function* loginFlow() {
 function* login(data) {
   try {
     const response = yield call(loginApi, data);
-    console.dir(response);
+    if (response.ok) {
+      response.json()
+        .then(tokenData => loginSuccess(tokenData));
+    }
   } catch (error) {
-
+    const errorMessage = `An error occurred when we tried to log you in.
+    Please check your network connection and try again`;
+    yield put({ type: actions.LOGIN_ERROR, payload: errorMessage });
   }
+}
+
+function* loginSuccess(tokenInformation) {
+  window.localStorage.setItem('ID_TOKEN', tokenInformation.id_token);
+  window.localStorage.setItem('ACCESS_TOKEN', tokenInformation.access_token);
+
+  yield put(push('/dashboard'));
 }
 
 export default loginFlow;

2991d50f146deb4c3fb7522cbb1500e51de2872c
Kent Kawahara <kent.kawahara@verys.com>
2017-12-08 12:59:26 -0800

    extracted data header html into smart/dumb components

--- src/components/data-header/DataHeader.js
+++ src/components/data-header/DataHeader.js
@@ -1,44 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Container from './components/container/Container';
+import Header from './components/header/Header';
+import Message from './components/message/Message';
+import ButtonGroup from './components/button-group/ButtonGroup';
+
 const dataHeader = props => (
-  <div className="row">
-    <div className="col-12 pt-3">
-      <div className="row align-items-center">
-        <div className="col-4 page-title-container">
-          <h1 className="page-title offset-top-10">
-            <span className="offset-top-8 mr-2">
-              <b>NUMBER OF {props.userType.toUpperCase()}</b>
-            </span>
-            <span className="font-xl">
-              <label id="numOfPlayers">{props.numberOfUsers}</label>
-            </span>
-          </h1>
-        </div>
-        <div className="col-4">
-          <div className="alert alert-info alert-dismissible fade show" hidden role="alert">
-            <button type="button" className="close" data-dismiss="alert" aria-label="Close"><span>&times;</span></button>
-            <h5>Form Message Box </h5>
-            <p>Shows messages after editing the information in the grid below.
-                </p>
-          </div>
-        </div>
-        <div className="col-4 text-right">
-          <a id="btn-export" className="btn btn-primary-02 mr-2 text-white" data-toggle="modal" data-target="#create-u">
-            <i className="fa fa-cloud-download mr-1" aria-hidden="true" /> EXPORT</a>
-          <a id="btn-import" className="btn btn-primary-02 text-white" ata-toggle="modal" ata-target="#importer">
-            <i className="fa fa-cloud-upload mr-1" aria-hidden="true" /> IMPORT</a>
-        </div>
-      </div>
-    </div>
-  </div>
+  <Container>
+    <Header
+      userType={props.userType}
+      numberOfUsers={props.numberOfUsers}
+    />
+    <Message
+      title={props.messageTitle}
+      message={props.messageBody}
+    />
+    <ButtonGroup />
+  </Container>
 );
 
 dataHeader.propTypes = {
   userType: PropTypes.string.isRequired,
-  numberOfUsers: PropTypes.number.isRequired
+  numberOfUsers: PropTypes.number.isRequired,
+  messageTitle: PropTypes.string,
+  messageBody: PropTypes.string
   // export: PropTypes.func.isRequired,
   // import: PropTypes.func.isRequired,
+};
+
+dataHeader.defaultProps = {
+  messageTitle: '',
+  messageBody: ''
 };
 
 export default dataHeader;
---
+++ src/components/data-header/components/button-group/ButtonGroup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const buttonGroup = props => (
+  <div className="col-4 text-right">
+    <a id="btn-export" className="btn btn-primary-02 mr-2 text-white" data-toggle="modal" data-target="#create-u">
+      <i className="fa fa-cloud-download mr-1" aria-hidden="true" /> EXPORT</a>
+    <a id="btn-import" className="btn btn-primary-02 text-white" ata-toggle="modal" ata-target="#importer">
+      <i className="fa fa-cloud-upload mr-1" aria-hidden="true" /> IMPORT</a>
+  </div>
+);
+
+export default buttonGroup;

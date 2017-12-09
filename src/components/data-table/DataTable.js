a391daf843358ed03a4bb2d7385e133d835255e8
Kent Kawahara <kent.kawahara@verys.com>
2017-12-08 19:07:58 -0800

    display loading component when getting data for players

--- src/components/data-table/DataTable.js
+++ src/components/data-table/DataTable.js
@@ -3,14 +3,18 @@ import PropTypes from 'prop-types';
 import uuidv4 from 'uuid/v4';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
+import Loading from './components/loading/Loading';
+
 const headerStyle = {
   textAlign: 'center',
   padding: '10px 20px'
 };
 
+const getNoDataText = loading => (loading ? <Loading /> : 'No data available');
+
 const dataTable = props => (
   <div className="row">
-    <BootstrapTable className="col-md-12" options={{ noDataText: 'This is custom text for empty data' }}>
+    <BootstrapTable className="col-md-12" options={{ noDataText: getNoDataText(props.loading) }}>
       {props.headers.map(header => (
         <TableHeaderColumn
           dataField={header.dataField}
@@ -28,12 +32,14 @@ const dataTable = props => (
 );
 
 dataTable.propTypes = {
+  loading: PropTypes.bool,
   data: PropTypes.array,
   headers: PropTypes.array.isRequired,
 };
 
 dataTable.defaultProps = {
-  data: []
+  data: [],
+  loading: true
 };
 
 export default dataTable;

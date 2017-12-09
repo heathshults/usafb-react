8ccdd6df7bf2e4aec6688451845b628e43fe62a3
Kent Kawahara <kent.kawahara@verys.com>
2017-12-08 17:00:02 -0800

    left comments in dropzone and login saga

--- src/components/dropzone/Dropzone.js
+++ src/components/dropzone/Dropzone.js
@@ -4,12 +4,15 @@ import Dropzone from 'react-dropzone';
 
 import Upload from './components/upload/Upload';
 
+// React drop-zone sets the borders as a style instead of class.
+// Once we add any className to the component, it gets completely overwritten
+// so we had to bring it in here.
 import './dropzone.css';
 
 const dropzone = props => (
   <Dropzone
     className="usafb__dropzone pt-0"
-    children={Upload}
+    children={Upload} // eslint-disable-line
   />
 );
 

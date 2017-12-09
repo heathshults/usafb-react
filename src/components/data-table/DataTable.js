a0a6524add5464bf5d0918cc207c07235e87990b
Kent Kawahara <kent.kawahara@verys.com>
2017-12-08 18:42:48 -0800

    updated table header styling so it matches Heath's template

--- src/components/data-table/DataTable.js
+++ src/components/data-table/DataTable.js
@@ -3,6 +3,11 @@ import PropTypes from 'prop-types';
 import uuidv4 from 'uuid/v4';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
+const headerStyle = {
+  textAlign: 'center',
+  padding: '10px 20px'
+};
+
 const dataTable = props => (
   <BootstrapTable options={{ noDataText: 'This is custom text for empty data' }}>
     {props.headers.map(header => (
@@ -11,6 +16,8 @@ const dataTable = props => (
         isKey={header.isKey}
         key={uuidv4()}
         dataSort={header.dataSort}
+        className="usafb__table-header"
+        thStyle={headerStyle}
       >
         {header.label.toUpperCase()}
       </TableHeaderColumn>

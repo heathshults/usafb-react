554938b8a101c9dfe7b5cda1adadb722df2a5e21
Kent Kawahara <kent.kawahara@verys.com>
2017-12-11 23:34:09 -0800

    updated bootstrap popover to render custom template

--- src/components/data-table-filter/components/filters/Filters.js
+++ src/components/data-table-filter/components/filters/Filters.js
@@ -1,22 +1,41 @@
+/* global $ */
+
 import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 // import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
 
+const displayFilters = () => (
+  <div>
+    Testing 123
+  </div>
+);
+
+$(function () {
+  $('[data-filter-toggle="popover"]').popover({
+    html: true,
+    content: ReactDOMServer.renderToString(displayFilters())
+  });
+});
+
 const filters = () => (
-  <button
-    tabIndex={0} //eslint-disable-line
-    className="data-table-filter__button"
-    data-toggle="popover"
-    data-trigger="focus"
-    title="Dismissible popover"
-    data-content="And here's some amazing content. It's very engaging. Right?"
-  >
-    <FontAwesome name="filter" />
-    <span className="data-table-filter__label">
-      Filters
+  <div>
+    <button
+      tabIndex={0} //eslint-disable-line
+      className="data-table-filter__button"
+      data-container="body"
+      data-filter-toggle="popover"
+      data-trigger="focus"
+      data-placement="bottom"
+      data-html="true"
+    >
+      <FontAwesome name="filter" />
+      <span className="data-table-filter__label">
+        Filters
     </span>
-    <FontAwesome name="caret-down" />
-  </button>
+      <FontAwesome name="caret-down" />
+    </button>
+  </div>
 );
 
 filters.propTypes = {

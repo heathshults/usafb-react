05b3af7146e5dd71e07fb28c1b749d9230d7c486
Kent Kawahara <kent.kawahara@verys.com>
2017-12-13 15:46:38 -0800

    display filter category in popover

--- src/components/data-table-filter/components/filters/Filters.js
+++ src/components/data-table-filter/components/filters/Filters.js
@@ -4,38 +4,48 @@ import ReactDOMServer from 'react-dom/server';
 import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
 
-const displayFilters = () => (
-  <div>
-    Testing 123
-  </div>
-);
+import './filters.css';
 
-$(function () {
-  $('[data-filter-toggle="popover"]').popover({
-    html: true,
-    content: ReactDOMServer.renderToString(displayFilters())
+const filters = (props) => {
+  const displayFilters = () => (
+    <div className="data-table-filter">
+      {props.filters.map(filter => (
+        <div key={filter.category}>
+          <h5 className="data-table-filter__category">
+            {filter.category}
+          </h5>
+        </div>
+      ))}
+    </div>
+  );
+
+  $(function () {
+    $('[data-filter-toggle="popover"]').popover({
+      html: true,
+      content: ReactDOMServer.renderToString(displayFilters())
+    });
   });
-});
 
-const filters = () => (
-  <div>
-    <button
-      tabIndex={0} //eslint-disable-line
-      className="data-table-filter__button"
-      data-container="body"
-      data-filter-toggle="popover"
-      data-trigger="focus"
-      data-placement="bottom"
-      data-html="true"
-    >
-      <FontAwesome name="filter" />
-      <span className="data-table-filter__label">
-        Filters
+  return (
+    <div>
+      <button
+        tabIndex={0} //eslint-disable-line
+        className="data-table-filter__button"
+        data-container="body"
+        data-filter-toggle="popover"
+        data-trigger="focus"
+        data-placement="bottom"
+        data-html="true"
+      >
+        <FontAwesome name="filter" />
+        <span className="data-table-filter__label">
+          Filters
     </span>
-      <FontAwesome name="caret-down" />
-    </button>
-  </div>
-);
+        <FontAwesome name="caret-down" />
+      </button>
+    </div>
+  );
+};
 
 filters.propTypes = {
   filters: PropTypes.array.isRequired

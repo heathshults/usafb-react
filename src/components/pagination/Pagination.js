7401b640393f00a581cd3d420bc34205788cdf3a
Kent Kawahara <kent.kawahara@verys.com>
2017-12-15 15:30:27 -0800

    pagination state has it's own state

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -1,37 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
 import Label from './components/label/Label';
 
-const pagination = props => (
-  <div className="row d-flex justify-content-between mt-3 mb-3">
-    <Label
-      currentPage={props.currentPage}
-      totalItems={props.totalItems}
-    />
-    <Pagination className="mb-0">
-      <PaginationItem>
-        <PaginationLink previous />
-      </PaginationItem>
-      {[...Array(20)].map((val, index) => (
-        <PaginationItem key={index}>
-          <PaginationLink>
-            {index}
-          </PaginationLink>
-        </PaginationItem>
-      ))}
-      <PaginationItem>
-        <PaginationLink next />
-      </PaginationItem>
-    </Pagination>
-  </div>
-);
+class PaginationComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      rowsPerPage: 10,
+      dropdownOpen: false
+    };
+  }
 
-pagination.propTypes = {
+  componentWillMount() {
+    console.dir(this.props);
+  }
+
+  toggleDropdown = () => this.setState({
+    dropdownOpen: !this.state.dropdownOpen
+  });
+
+  updateRowsPerPage = (event) => {
+    this.setState({
+      rowsPerPage: +event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="row d-flex justify-content-between mt-3 mb-3">
+        <Label
+          currentPage={this.props.currentPage}
+          totalItems={this.props.totalItems}
+          rowsPerPage={this.state.rowsPerPage}
+          dropdownOpen={this.state.dropdownOpen}
+          toggleDropdown={this.toggleDropdown}
+          updateRowsPerPage={this.updateRowsPerPage}
+        />
+        <Pagination className="mb-0">
+          <PaginationItem>
+            <PaginationLink previous />
+          </PaginationItem>
+          {[...Array(20)].map((val, index) => (
+            <PaginationItem key={index}>
+              <PaginationLink>
+                {index}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          <PaginationItem>
+            <PaginationLink next />
+          </PaginationItem>
+        </Pagination>
+      </div>
+    );
+  }
+}
+
+PaginationComponent.propTypes = {
   currentPage: PropTypes.number.isRequired,
   totalItems: PropTypes.number.isRequired
 };
 
-export default pagination;
+export default PaginationComponent;

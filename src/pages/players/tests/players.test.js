1bb70feae2a48b2c4ea8fbbb1f864d5d91600b09
Kent Kawahara <kent.kawahara@verys.com>
2017-12-18 15:49:48 -0800

    created players pane acceptance test

--- src/pages/players/tests/players.test.js
+++ src/pages/players/tests/players.test.js
@@ -4,38 +4,25 @@ import renderer from 'react-test-renderer';
 
 import nightmareRedirect from 'services/testing/nightmare';
 
-import Players from '../Players';
-import Container from 'components/containers/Container';
-import DataHeader from 'components/data-header/DataHeader';
-import ImportModal from '../components/import-modal/ImportModal';
-import DataTable from 'components/data-table/DataTable';
-
-const setupPlayerComponent = () => {
-  const props = {};
-  const playerWrapper = mount(<Players {...props} />);
-
-  return {
-    props,
-    playerWrapper
-  };
-}
-
-describe('[Players Pane] UI components test', () => {
-  const { playerWrapper } = setupPlayerComponent();
-
-  test('Container exists', () => {
-    expect(playerWrapper.find(Container).length).toBe(1);
-  });
-
-  test('DataHeader exists', () => {
-    expect(playerWrapper.find(DataHeader).length).toBe(1);
-  });
-
-  test('ImportModal exists', () => {
-    expect(playerWrapper.find(ImportModal).length).toBe(1);
-  });
-
-  test('DataTable exists', () => {
-    expect(playerWrapper.find(DataTable).length).toBe(1);
+describe('[PLAYERS PANE] acceptance test', () => {
+  test('login page acceptance test', async function () {
+    const page = await nightmareRedirect('/players')
+      .click('button#filterPopoverButton > span.data-table-filter__label:nth-child(2)')
+      .wait(100)
+      .click('input#Organization')
+      .wait(100)
+      .click('div#root')
+      .wait(100)
+      .evaluate(() =>
+        document.querySelector('section#main-content > div.container-fluid:nth-child(1) > div.row:nth-child(4) > div.react-bs-table-container.col-md-12:nth-child(1) > div.react-bs-table.react-bs-table-bordered:nth-child(1) > div.react-bs-container-header.table-header-wrapper:nth-child(1) > table.table.table-hover.table-bordered:nth-child(1) > thead:nth-child(2) > tr:nth-child(1) > th.usafb__table-header.sort-column:nth-child(7)').innerHTML
+      )
+      .end()
+      .then((result) => {
+        const value = result.substring(0, 9);
+        expect(value).toBe('Age Group');
+      })
+      .catch(function (error) {
+        console.error('Error:', error);
+      });
   });
 });

ec1221f423097c8421c8bb5b580aa53a5ded16f1
Kent Kawahara <kent.kawahara@verys.com>
2017-12-18 16:39:47 -0800

    created test for advanced search on players pane

--- src/pages/players/tests/players.test.js
+++ src/pages/players/tests/players.test.js
@@ -5,7 +5,7 @@ import renderer from 'react-test-renderer';
 import nightmareRedirect from 'services/testing/nightmare';
 
 describe('[PLAYERS PANE] acceptance test', () => {
-  test('login page acceptance test', async function () {
+  test('filtering columns', async function () {
     const page = await nightmareRedirect('/players')
       .click('button#filterPopoverButton > span.data-table-filter__label:nth-child(2)')
       .wait(100)
@@ -24,5 +24,19 @@ describe('[PLAYERS PANE] acceptance test', () => {
       .catch(function (error) {
         console.error('Error:', error);
       });
+  });
+
+  // this test should just end without crashing
+  test('advanced search', async function () {
+    const page = await nightmareRedirect('/players')
+      .wait('section#main-content > div.container-fluid:nth-child(1) > div.row:nth-child(3) > div.col-md-12.btn-group.data-table-filter:nth-child(1) > div:nth-child(2) > button.data-table-filter__button:nth-child(1) > span.data-table-filter__label:nth-child(2)')
+      .click('section#main-content > div.container-fluid:nth-child(1) > div.row:nth-child(3) > div.col-md-12.btn-group.data-table-filter:nth-child(1) > div:nth-child(2) > button.data-table-filter__button:nth-child(1) > span.data-table-filter__label:nth-child(2)')
+      .wait(100)
+      .click('input#advancedsearch-Source')
+      .wait(100)
+      .type('input#advancedsearch-Source', 'Source')
+      .wait(100)
+      .click('html > body.modal-open > div:nth-child(7) > div:nth-child(1) > div.modal.fade.show:nth-child(1) > div.modal-dialog:nth-child(1) > div.modal-content:nth-child(1) > div.modal-header:nth-child(1) > button.close:nth-child(2) > span:nth-child(1)')
+      .end()
   });
 });

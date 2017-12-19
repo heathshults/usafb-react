deb80957a1f3fe8d6eb7b786f8d464d42b1c5626
Kent Kawahara <kent.kawahara@verys.com>
2017-12-18 20:43:55 -0800

    created coaches page and linked it to routes

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -8,6 +8,7 @@ import store from 'services/redux/store';
 import Login from 'pages/login/Login';
 import Players from 'pages/players/Players';
 import Dashboard from 'pages/dashboard/Dashboard';
+import Coaches from 'pages/coaches/Coaches';
 
 import './app.css';
 
@@ -17,9 +18,10 @@ const app = () => (
   <Provider store={store}>
     <Router history={history}>
       <Switch>
-        <Route exact path="/players" component={Players} />
-        <Route exact path="/login" component={Login} />
         <Route exact path="/" component={Dashboard} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/players" component={Players} />
+        <Route exact path="/coaches" component={Coaches} />
       </Switch>
     </Router>
   </Provider>
---
+++ src/pages/coaches/Coaches.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Coaches extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        Coaches page
+      </div>
+    );
+  }
+}
+
+export default Coaches;

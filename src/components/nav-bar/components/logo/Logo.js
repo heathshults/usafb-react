9f2f2be4ea2136ca2117c6a65d6b465da51920c6
Kent Kawahara <kent.kawahara@verys.com>
2017-12-20 15:59:26 -0800

    created logo component for nav bar

---
+++ src/components/nav-bar/components/logo/Logo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logoImage from 'images/logo/usafb-logo.png';
+
+import './logo.css';
+
+const logo = () => (
+  <div className="nav-bar__logo-container">
+    <img
+      src={logoImage}
+      alt="USAFB"
+      className="nav-bar__logo"
+    />
+    <div className="nav-bar__header">
+      <h4>
+        National Database
+      </h4>
+    </div>
+  </div>
+);
+
+export default logo;

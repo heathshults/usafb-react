e80cc9ca4c7fa1c4e50fdd65a78bd8ee683d24a3
Kent Kawahara <kent.kawahara@verys.com>
2017-12-21 15:19:36 -0800

    removed unnecessary id for header

--- src/components/data-header/components/header/Header.js
+++ src/components/data-header/components/header/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// Setting an ID to <b> element so it can be easier to access for testing [KK]
 const header = props => (
   <div className="col-4 page-title-container">
     <h1 className="page-title offset-top-10">
       <span className="offset-top-8 mr-2">
-        <b id="data-header-label">NUMBER OF {props.userType.toUpperCase()}</b>
+        <b>NUMBER OF {props.userType.toUpperCase()}</b>
       </span>
       <span className="font-xl">
         <label id="numOfPlayers">{props.numberOfUsers}</label>

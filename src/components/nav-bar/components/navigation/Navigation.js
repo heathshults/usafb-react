31e958313c8987fbc614c3f221409c5d9fd40298
Kent Kawahara <kent.kawahara@verys.com>
2017-12-21 10:12:40 -0800

    navbar links updated

--- src/components/nav-bar/components/navigation/Navigation.js
+++ src/components/nav-bar/components/navigation/Navigation.js
@@ -1,23 +1,25 @@
 import React from 'react';
 
+import './navigation.css';
+
 const navigation = () => (
-  <nav aria-label="breadcrumb">
-    <ol className="breadcrumb">
-      <li className="breadcrumb-item">
-        <a>
-          Home
-        </a>
-      </li>
-      <li className="breadcrumb-item">
-        <a>
-          Library
-        </a>
-      </li>
-      <li className="breadcrumb-item active" aria-current="page">
-        Data
-      </li>
-    </ol>
-  </nav>
+  <ul className="d-flex nav-bar__navigation mr-auto mt-2 mt-lg-0 ml-auto">
+    <li>
+      <a className="nav-bar__link" href="./dashboard.html">Home</a>
+    </li>
+    <span className="nav-bar__separator" />
+    <li>
+      <a className="nav-bar__link active" href="./players-list.html">Players</a>
+    </li>
+    <span className="nav-bar__separator" />
+    <li>
+      <a className="nav-bar__link" href="./coaches-list.html">Coaches</a>
+    </li>
+    <span className="nav-bar__separator" />
+    <li>
+      <a className="nav-bar__link" href="./dashboard01.html">Dashboard</a>
+    </li>
+  </ul>
 );
 
 export default navigation;

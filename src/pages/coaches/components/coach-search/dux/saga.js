0a8bd9986a409c660fa148c8c7e52daff79cce63
Michael Mitrakos <mike.mitrakos@verys.com>
2018-01-02 13:05:12 -0500

    [feature] USAFB-307 coach/player search sagas

---
+++ src/pages/coaches/components/coach-search/dux/saga.js
@@ -0,0 +1,19 @@
+import { take, call, put } from 'redux-saga/effects'; //eslint-disable-line
+
+import * as actions from './actions';
+import searchCoaches from './api';
+
+export default function* coachSearchFlow() {
+  while (true) {
+    try {
+      const { data } = yield take(actions.SEARCH_COACHES);
+      const response = yield call(searchCoaches, data);
+      const responseData = yield response.json();
+      if (response.ok) {
+        yield put({ type: actions.SEARCH_COACHES_SUCCESS, coachSearchData: responseData.data });
+      }
+    } catch (e) {
+      console.log('eeee', e); //eslint-disable-line
+    }
+  }
+}
---
+++ src/pages/players/components/player-search/dux/saga.js
@@ -0,0 +1,19 @@
+import { take, call, put } from 'redux-saga/effects'; //eslint-disable-line
+
+import * as actions from './actions';
+import searchPlayer from './api';
+
+export default function* playerSearchFlow() {
+  while (true) {
+    try {
+      const { data } = yield take(actions.SEARCH_PLAYERS);
+      const response = yield call(searchPlayer, data);
+      const responseData = yield response.json();
+      if (response.ok) {
+        yield put({ type: actions.SEARCH_PLAYERS_SUCCESS, playerSearchData: responseData.data });
+      }
+    } catch (e) {
+      console.log('eeee', e); //eslint-disable-line
+    }
+  }
+}

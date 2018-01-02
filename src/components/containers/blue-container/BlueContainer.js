72ba734e92644be22e931bd3cb6b132548ac1351
Kent Kawahara <kent.kawahara@verys.com>
2018-01-02 10:50:54 -0800

    created blue container

--- src/components/containers/blue-container/BlueContainer.js
+++ src/components/containers/blue-container/BlueContainer.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './blue-container.css';
+
 const blueContainer = props => (
-  <section>
+  <section className="usafb__blue-container">
     {React.Children.map(props.children, child => child)}
   </section>
 );

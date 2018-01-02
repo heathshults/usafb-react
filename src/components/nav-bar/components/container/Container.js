e019c2a5038afd7ef7f5df02fae48e8d90bf1e33
Kent Kawahara <kent.kawahara@verys.com>
2018-01-02 12:11:13 -0800

    updated padding on nav bar

--- src/components/nav-bar/components/container/Container.js
+++ src/components/nav-bar/components/container/Container.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import './container.css';
 
 const container = props => (
-  <header className="row justify-space-between p-4 nav-bar">
+  <header className="row justify-space-between pt-3 pb-3 pr-5 pl-5 nav-bar">
     {React.Children.map(props.children, child => child)}
   </header>
 );

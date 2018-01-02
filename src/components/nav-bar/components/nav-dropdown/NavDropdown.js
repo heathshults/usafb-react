3d8c031c5e52ccadd716d52ae08360e5ecabf452
Kent Kawahara <kent.kawahara@verys.com>
2018-01-02 14:09:21 -0800

    created navdropdown component

---
+++ src/components/nav-bar/components/nav-dropdown/NavDropdown.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { ButtonDropdown, DropdownToggle, DropdownMenu } from 'reactstrap';
+// import PropTypes from 'prop-types';
+
+import './nav-dropdown.css';
+
+class NavDropdown extends Component {
+  constructor() {
+    super();
+    this.state = {
+      open: false
+    };
+  }
+
+  toggle = () => this.setState({
+    open: !this.state.open
+  });
+
+  render() {
+    return (
+      <ButtonDropdown
+        isOpen={this.state.open}
+        toggle={this.toggle}
+        className="nav-bar__dropdown"
+      >
+        <DropdownToggle
+          caret
+          className="nav-bar__dropdown-button"
+        >
+          {this.props.label}
+        </DropdownToggle>
+        <DropdownMenu>
+          {React.Children.map(this.props.children, child => child)}
+        </DropdownMenu>
+      </ButtonDropdown>
+    );
+  }
+}
+
+NavDropdown.propTypes = {
+  label: PropTypes.string.isRequired,
+  children: PropTypes.array.isRequired
+};
+
+export default NavDropdown;

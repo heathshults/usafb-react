6a69850478d3f3241580f831005a3b8b1c8dbf6a
Kent Kawahara <kent.kawahara@verys.com>
2018-01-09 13:22:50 -0800

    created landing page component and linked to routes

---
+++ src/pages/landing/Landing.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import Container from 'components/containers/blue-container/BlueContainer';
+
+class Landing extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Container>
+        this is a landing page
+      </Container>
+    );
+  }
+}
+
+export default Landing;

2096d745140dd07cf9a9a37d32ab35c8ef03ec6c
Kent Kawahara <kent.kawahara@verys.com>
2018-01-09 15:09:29 -0800

    display bar chart

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Bar } from 'recharts';
 
 import Container from 'components/containers/blue-container/BlueContainer';
 import Content from './components/content/Content';
 import Header from './components/header/Header';
 
+import { barGraph } from './models/dummyData';
 import './landing.css';
 
 class Landing extends Component {
@@ -17,7 +19,17 @@ class Landing extends Component {
       <Container className="landing__container">
         <Content>
           <Header count={100000} header="players" />
-          <div />
+          <BarChart
+            data={barGraph}
+            height={300}
+            width={700}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis dataKey="value" />
+            <Tooltip />
+            <Bar dataKey="value" fill="#8884d8" />
+          </BarChart>
         </Content>
         <Content>
           <Header count={100000} header="players" />
---
+++ src/pages/landing/models/dummyData.js
@@ -0,0 +1,14 @@
+export const barGraph = [
+  {
+    name: 'a',
+    value: 12
+  },
+  {
+    name: 'b',
+    value: 8
+  }
+];
+
+export const pieChart = [
+
+];

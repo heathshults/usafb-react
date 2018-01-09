d78f5e8e7304658bb8d8ad2c294e8bc55f2d7b22
Kent Kawahara <kent.kawahara@verys.com>
2018-01-09 11:38:40 -0800

    added header to the form

--- src/pages/login/components/form/Form.js
+++ src/pages/login/components/form/Form.js
@@ -9,6 +9,7 @@ const form = props => (
   <div className="col-md-4 text-center">
     <Logo />
     <div id="si-form" className="width-si-form">
+      <h1>NATIONAL DATABASE</h1>
       {React.Children.map(props.children, child => child)}
     </div>
   </div>

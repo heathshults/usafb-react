131366ec45f17c4910cdabbd1f34a0cb6caa4171
Michael Mitrakos <mike.mitrakos@verys.com>
2018-01-11 14:48:18 -0800

    [feature] USAFB-415 progress on drag and drop

--- src/pages/coach-export-modal/components/drag-and-drop/DragAndDrop.js
+++ src/pages/coach-export-modal/components/drag-and-drop/DragAndDrop.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Draggable, Droppable } from 'react-drag-and-drop';
+import './dragAndDrop.css';
 
 const onDrop = (data) => {
   console.log(data); // eslint-disable-line
 };
 
 const DragAndDrop = () => (
-  <div>
+  <div className="dragAndDrop">
     <ul>
       <Draggable type="fruit" data="banana"><li>Banana</li></Draggable>
       <Draggable type="fruit" data="apple"><li>Apple</li></Draggable>
       <Draggable type="metal" data="silver"><li>Silver</li></Draggable>
     </ul>
     <Droppable
+      className="drop-area"
       types={['fruit']} // <= allowed drop types
       onDrop={onDrop}
-      style={{ backgroundColor: 'red' }}
-    // <ul className="Smoothie" />
-    />
+    >
+      <ul className="Smoothie" />
+    </Droppable>
   </div>
 );
 

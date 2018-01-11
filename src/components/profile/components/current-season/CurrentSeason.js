7a02c9e193d6da249e24570e15e373a07585efdd
Kent Kawahara <kent.kawahara@verys.com>
2018-01-11 10:28:47 -0800

    added current season scss files

--- src/components/profile/components/current-season/CurrentSeason.js
+++ src/components/profile/components/current-season/CurrentSeason.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './current-season.css';
+
 const CurrentSeason = props => (
   <span>
     {/* eslint-disable */}
@@ -12,9 +14,9 @@ const CurrentSeason = props => (
         <h4 className="mb-4">{props.currentTeam.team_name}
           <small className="ml-2">({props.currentTeam.school_name})</small>
         </h4>
-        <div className="tats-wrapper">
-          <div className="row text-center mt-2">
-            <div className="col-3 current-stats">
+        <div className="current-season__stats-wrapper">
+          <div className="row text-center align-items-center mt-2">
+            <div className="col-3 current-stats current-season__stats">
               <div className="stat-title divider-br">
                 {props.currentTeam.positions && props.currentTeam.positions.map(position => (
                   <div className="positions-list" key={`${props.currentTeam._id}+${position}`}>
@@ -23,13 +25,13 @@ const CurrentSeason = props => (
                 ))}
               </div>
             </div>
-            <div className="col-3 current-stats">
+            <div className="col-3 current-stats current-season__stats">
               <div className="stat-title divider-br">{props.currentTeam.level_type}</div>
             </div>
-            <div className="col-3 current-stats">
+            <div className="col-3 current-stats current-season__stats">
               <div className="stat-title divider-br">{props.currentTeam.organization_name}</div>
             </div>
-            <div className="col-3 current-stats">
+            <div className="col-3 current-stats current-season__stats">
               <div className="stat-title ">{props.currentTeam.season} {props.currentTeam.season_year}</div>
             </div>
           </div>

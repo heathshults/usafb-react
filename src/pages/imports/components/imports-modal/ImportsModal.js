9e78bedb905e1713b3dadd06361fb552ca671b9b
Kent Kawahara <kent.kawahara@verys.com>
2018-01-12 10:58:46 -0800

    added text to dropzone

--- src/pages/imports/components/imports-modal/ImportsModal.js
+++ src/pages/imports/components/imports-modal/ImportsModal.js
@@ -4,13 +4,17 @@ import PropTypes from 'prop-types';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import Dropzone from 'react-dropzone';
 
+import './imports-modal.css';
+
 const importsModal = props => (
   <Modal isOpen={props.open} toggle={props.toggle} >
     <ModalHeader toggle={props.toggle}>Modal title</ModalHeader>
-    <ModalBody>
-      <Dropzone
-        onDrop={props.onDrop}
-      />
+    <ModalBody className="imports-modal__body">
+      <Dropzone onDrop={props.onDrop} multiple={false}>
+        <p className="text-center mt-4">
+          Click here, or drag items to upload a svg file
+        </p>
+      </Dropzone>
     </ModalBody>
     <ModalFooter>
       <Button color="primary" onClick={props.toggle}>Do Something</Button>{' '}

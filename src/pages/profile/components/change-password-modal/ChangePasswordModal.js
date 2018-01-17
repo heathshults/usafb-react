4832519cbaaf3a107fd91ac26571b1b87ba6e546
Kent Kawahara <kent.kawahara@verys.com>
2018-01-16 17:04:53 -0800

    created change password input field

--- src/pages/profile/components/change-password-modal/ChangePasswordModal.js
+++ src/pages/profile/components/change-password-modal/ChangePasswordModal.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Label, Input } from 'reactstrap';
+
+import './change-password-modal.css';
 
 const changePassword = props => (
   <Modal isOpen={props.open} >
     <ModalHeader toggle={props.cancel}>Change your password</ModalHeader>
     <ModalBody>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <div className="d-flex flex-column mr-2 ml-2">
+        <Label for="currentPassword">Current Password</Label>
+        <Input
+          type="password"
+          name="password"
+          id="currentPassword"
+          className="change-password__input-field w-100"
+        />
+      </div>
+      <div className="d-flex flex-column mr-2 ml-2 mt-2">
+        <Label for="newPassword">New Password</Label>
+        <Input
+          type="password"
+          name="password"
+          id="newPassword"
+          className="change-password__input-field w-100"
+        />
+      </div>
     </ModalBody>
     <ModalFooter>
       <Button color="secondary" onClick={props.cancel}>Cancel</Button>&nbsp;

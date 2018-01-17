79fc4aa50c4ad60e300f63132c144c6fa8316230
Michael Mitrakos <mike.mitrakos@verys.com>
2018-01-17 12:15:46 -0800

    [feature] USAFB-permissions add config so can view real-time redux store with extension

--- src/services/redux/store.js
+++ src/services/redux/store.js
@@ -5,9 +5,12 @@ import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
+/* eslint-disable no-underscore-dangle */
 export default createStore(
   reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(sagaMiddleware)
 );
+/* eslint-enable */
 
 sagaMiddleware.run(rootSaga);

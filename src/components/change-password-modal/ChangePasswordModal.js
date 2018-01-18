74aedbebfd09d2ea1318f7c23ba658fdc3d265a7
Kent Kawahara <kent.kawahara@verys.com>
2018-01-18 13:51:36 -0800

    made ChangePasswordModal to class instead of FSC

--- src/components/change-password-modal/ChangePasswordModal.js
+++ src/components/change-password-modal/ChangePasswordModal.js
@@ -1,87 +1,91 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Label, Input, FormText } from 'reactstrap';
 
 import './change-password-modal.css';
 
-const getChangePasswordLabel = (changing) => {
-  if (changing) {
+class ChangePasswordModal extends Component {
+  getChangePasswordLabel = () => {
+    if (this.props.changingPassword) {
+      return (
+        <span>
+          <i className="fa fa-spinner fa-spin" />&nbsp;
+            saving...
+        </span>
+      );
+    }
+
+    return 'Change Password';
+  };
+
+  getButtonDisabledStatus = () => {
+    if (this.props.currentPasswordRequired) {
+      return (!this.props.currentPassword || !this.props.newPassword || !this.props.confirmNewPassword) || (this.props.confirmNewPassword !== this.props.newPassword) || this.props.changingPassword;
+    }
+
+    return (!this.props.newPassword || !this.props.confirmNewPassword) || (this.props.confirmNewPassword !== this.props.newPassword) || this.props.changingPassword;
+  };
+
+  render() {
     return (
-      <span>
-        <i className="fa fa-spinner fa-spin" />&nbsp;
-          saving...
-      </span>
+      <Modal isOpen={this.props.open} >
+        <ModalHeader toggle={this.props.cancel}>Change your password</ModalHeader>
+        <ModalBody>
+          {this.props.currentPasswordRequired &&
+            <div className="d-flex flex-column mr-2 ml-2">
+              <Label for="currentPassword">Current Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="currentPassword"
+                className="change-password__input-field w-100"
+                value={this.props.currentPassword}
+                onChange={this.props.updateCurrentPassword}
+              />
+            </div>
+          }
+          <div className="d-flex flex-column mr-2 ml-2 mt-2">
+            <Label for="newPassword">New Password</Label>
+            <Input
+              type="password"
+              name="password"
+              id="newPassword"
+              className="change-password__input-field w-100"
+              value={this.props.newPassword}
+              onChange={this.props.updateNewPassword}
+            />
+          </div>
+          <div className="d-flex flex-column mr-2 ml-2 mt-2">
+            <Label for="confirmNewPassword">Confirm New Password</Label>
+            <Input
+              type="password"
+              name="password"
+              id="confirmNewPassword"
+              className="change-password__input-field w-100"
+              value={this.props.confirmNewPassword}
+              onChange={this.props.updateConfirmNewPassword}
+            />
+          </div>
+          <FormText className="mt-2 text-center">Passwords must include a capital letter, a special letter and a number</FormText>
+        </ModalBody>
+        <ModalFooter>
+          {this.props.hideCancelButton &&
+            <Button color="secondary mr-2" onClick={this.props.cancel}>Cancel</Button>
+          }
+          <Button
+            color="primary"
+            onClick={this.props.setPassword}
+            disabled={this.getButtonDisabledStatus()}
+          >
+            {this.getChangePasswordLabel()}
+          </Button>
+        </ModalFooter>
+      </Modal>
     );
   }
+}
 
-  return 'Change Password';
-};
-
-const getButtonDisabledStatus = (props) => {
-  if (props.currentPasswordRequired) {
-    return (!props.currentPassword || !props.newPassword || !props.confirmNewPassword) || (props.confirmNewPassword !== props.newPassword) || props.changingPassword;
-  }
-
-  return (!props.newPassword || !props.confirmNewPassword) || (props.confirmNewPassword !== props.newPassword) || props.changingPassword;
-};
-
-const changePassword = props => (
-  <Modal isOpen={props.open} >
-    <ModalHeader toggle={props.cancel}>Change your password</ModalHeader>
-    <ModalBody>
-      {props.currentPasswordRequired &&
-        <div className="d-flex flex-column mr-2 ml-2">
-          <Label for="currentPassword">Current Password</Label>
-          <Input
-            type="password"
-            name="password"
-            id="currentPassword"
-            className="change-password__input-field w-100"
-            value={props.currentPassword}
-            onChange={props.updateCurrentPassword}
-          />
-        </div>
-      }
-      <div className="d-flex flex-column mr-2 ml-2 mt-2">
-        <Label for="newPassword">New Password</Label>
-        <Input
-          type="password"
-          name="password"
-          id="newPassword"
-          className="change-password__input-field w-100"
-          value={props.newPassword}
-          onChange={props.updateNewPassword}
-        />
-      </div>
-      <div className="d-flex flex-column mr-2 ml-2 mt-2">
-        <Label for="confirmNewPassword">Confirm New Password</Label>
-        <Input
-          type="password"
-          name="password"
-          id="confirmNewPassword"
-          className="change-password__input-field w-100"
-          value={props.confirmNewPassword}
-          onChange={props.updateConfirmNewPassword}
-        />
-      </div>
-      <FormText className="mt-2 text-center">Passwords must include a capital letter, a special letter and a number</FormText>
-    </ModalBody>
-    <ModalFooter>
-      {props.hideCancelButton &&
-        <Button color="secondary mr-2" onClick={props.cancel}>Cancel</Button>
-      }
-      <Button
-        color="primary"
-        onClick={props.setPassword}
-        disabled={getButtonDisabledStatus(props)}
-      >
-        {getChangePasswordLabel(props.changingPassword)}
-      </Button>
-    </ModalFooter>
-  </Modal>
-);
-
-changePassword.propTypes = {
+ChangePasswordModal.propTypes = {
   open: PropTypes.bool.isRequired,
   cancel: PropTypes.func,
   currentPassword: PropTypes.string,
@@ -96,7 +100,7 @@ changePassword.propTypes = {
   hideCancelButton: PropTypes.bool
 };
 
-changePassword.defaultProps = {
+ChangePasswordModal.defaultProps = {
   currentPassword: '',
   currentPasswordRequired: true,
   updateCurrentPassword: () => { },
@@ -104,4 +108,4 @@ changePassword.defaultProps = {
   hideCancelButton: false
 };
 
-export default changePassword;
+export default ChangePasswordModal;

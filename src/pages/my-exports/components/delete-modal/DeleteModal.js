8df8bc8a9de2ccb65098b662486aab1c79fb1495
Michael Mitrakos <mike.mitrakos@verys.com>
2018-01-23 10:45:36 -0800

    [feature] USAFB-bug-27 modal now works properly

--- src/pages/my-exports/components/delete-modal/DeleteModal.js
+++ src/pages/my-exports/components/delete-modal/DeleteModal.js
@@ -9,8 +9,8 @@ const deleteModal = props => (
       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </ModalBody>
     <ModalFooter>
-      <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-      <Button color="primary" onClick={this.toggle}>Delete</Button>{' '}
+      <Button color="secondary" onClick={props.toggle}>Cancel</Button>
+      <Button color="primary" onClick={props.toggle}>Delete</Button>{' '}
     </ModalFooter>
   </Modal>
 );

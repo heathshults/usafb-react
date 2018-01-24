e51d302df5e28b6ca18f378566238924322a8f38
Michael Mitrakos <mike.mitrakos@verys.com>
2018-01-24 10:17:25 -0800

    [feature] USAFB-profileChanges fix profile position changes

--- src/pages/player-profile/components/past-season/PastSeason.js
+++ src/pages/player-profile/components/past-season/PastSeason.js
@@ -21,12 +21,8 @@ const PastSeason = props => (
                     </h4>
                     <div className="stats-wrapper past-season__stats-wrapper">
                       <div className="d-flex justify-content-between">
-                        <div className="stat-title current-season__stats text-center divider-br p-4 mt-2 mb-2">
-                          {team.positions && team.positions.map(position => (
-                            <div className="positions-list" key={`${team._id}+${position}`}>
-                              <p style={{ wordBreak: 'break-word' }}>{humanize(position)}</p>
-                            </div>
-                          ))}
+                        <div className="col-3 current-stats past-season__stats">
+                          <div className="stat-title divider-br">{humanize(team.position)}</div>
                         </div>
                         <div className="stat-title current-season__stats text-center divider-br p-4 mt-2 mb-2">
                           <div className="badge-grade ar-1by1"> {props.playerData.grade}th

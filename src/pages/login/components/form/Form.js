e3025606336dcd2d658ae0d748123a81a5928c0a
heathshults <heatheshults@gmail.com>
2018-01-29 14:26:56 -0600

    css styles and positions for signin page

--- src/pages/login/components/form/Form.js
+++ src/pages/login/components/form/Form.js
@@ -10,7 +10,7 @@ const form = props => (
     <div className="sign-in-form-container text-center">
       <Logo />
       <div id="si-form" className="sign-in-form">
-        <h1 className="page-title text-light font-80" id="fadeInH1">NATIONAL DATABASE</h1>
+        <h1 className="page-title text-light font-50" id="fadeInH1">NATIONAL DATABASE</h1>
         {React.Children.map(props.children, child => child)}
       </div>
     </div>
--- src/pages/login/components/remember-me/RememberMe.js
+++ src/pages/login/components/remember-me/RememberMe.js
@@ -2,9 +2,9 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 const rememberMe = () => (
-  <div className="row form-group d-inline-flex">
-    <div className="col-md-12 input-group justify-content-center">
-      <label htmlFor="rememberMe" className="form-check-label">
+  <div className="row form-group d-inline-flex mt-0 pt-0">
+    <div className="col-md-12 input-group justify-content-center mt-0 pt-0">
+      <label htmlFor="rememberMe" className="form-check-label mt-0 pt-0">
         <span className="checkbox-theme">
           <input
             id="rememberMe"

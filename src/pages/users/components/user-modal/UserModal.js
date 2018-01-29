77996edd700f4b69071d7e04185cb4b239be6089
Kent Kawahara <kent.kawahara@verys.com>
2018-01-29 13:34:44 -0800

    added an email validator

--- src/pages/users/components/user-modal/UserModal.js
+++ src/pages/users/components/user-modal/UserModal.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 import states from 'services/data/states';
+import { validateEmail } from 'services/validations/validation';
 
 import InputField from '../input-field/InputField';
 import DropdownField from '../dropdown-field/DropdownField';
@@ -95,10 +96,15 @@ class UserModal extends Component {
       name_last: event.target.value
     });
 
-  updateEmail = event =>
+  updateEmail = (event) => {
     this.setState({
       email: event.target.value
-    });
+    }, this.validateEmail);
+  }
+
+  validateEmail = () => {
+    console.log(validateEmail(this.state.email)); //eslint-disable-line
+  }
 
   updateRole = event =>
     this.setState({

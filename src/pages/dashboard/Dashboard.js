2a1efc7d197bf52ec4f8e80784ff32fbfa035060
Michael Mitrakos <mike.mitrakos@verys.com>
2018-01-30 12:47:11 -0800

    [feature] USAFB-remove log

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -9,7 +9,6 @@ export default class dashboard extends Component {
 
   render() {
     const iframeUrl = this.props.location.pathname.includes('coaches') ? process.env.REACT_APP_PERISCOPE_COACH_DASHBOARD_URL : process.env.REACT_APP_PERISCOPE_PLAYER_DASHBOARD_URL;
-    console.log('iframeUrl', iframeUrl); //eslint-disable-line
     return (
       <section id="main-content">
         <div className="container">
